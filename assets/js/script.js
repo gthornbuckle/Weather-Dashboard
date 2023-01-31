11ce3c868a93a89729db08419d286ffe5210624a
gthornbuckle <gthornbuckle@gmail.com>
2023-01-31 20:17:40 +0000

    Adds functionaliy to create HTML for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 $("#search-button").on("click", function() {
-    var userSearchInput = $("#search-input").val();
+
+     $("#forecast").empty(); //Clears any existing forecast HTML.
+
+    var userSearchInput = $("#search-input").val(); //Takes user input
     
-    var histoyBtn = $("<button>").text(userSearchInput);
+    var histoyBtn = $("<button>").text(userSearchInput); //Creates a button with the user input
     histoyBtn.addClass("btn");
+    histoyBtn.attr("id", "btnHistory");
     $("#history").prepend(histoyBtn);
+
     // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+userSearchInput+ "&appid=5d69237f779c6ad1becd9e8ed5e67f08";
 
     // $.ajax({
@@ -13,7 +18,21 @@ $("#search-button").on("click", function() {
 
     //     console.log(response);
     //   })
-    for (var i = 0; i < weatherInfo.length; i++){
+
+    for (var i = 0; i < weatherInfo.length; i++){ //Creates 5 day forecast HTML
+
+        var currentWeather = $("<div>");
+        var heading = $("<h3>").text(weatherInfo[i].city+ " ("+ weatherInfo[i].date+ ")"+ weatherInfo[i].icon);
+        var temperature = $("<p>").text("Temperature: " + weatherInfo[i].temperature);
+        var humidity = $("<p>").text("Humidity: " + weatherInfo[i].humidity);
+        var windspeed = $("<p>").text("Wind Speed: " + weatherInfo[i].windspeed);
+        
+        currentWeather.append(heading);
+        currentWeather.append(temperature);
+        currentWeather.append(humidity);
+        currentWeather.append(windspeed);
+
+        $("#forecast").prepend(currentWeather);
 
     }
 });
