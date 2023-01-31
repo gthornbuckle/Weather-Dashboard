e9fb78ad3826cf0b958a46ff920af57af6f7b3fc
gthornbuckle <gthornbuckle@gmail.com>
2023-01-31 19:58:00 +0000

    Adds functionality to save user search input as a button so that the search can be recalled later without input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 $("#search-button").on("click", function() {
     var userSearchInput = $("#search-input").val();
     
+    var histoyBtn = $("<button>").text(userSearchInput);
+    histoyBtn.addClass("btn");
+    $("#history").prepend(histoyBtn);
     // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+userSearchInput+ "&appid=5d69237f779c6ad1becd9e8ed5e67f08";
-      var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=5d69237f779c6ad1becd9e8ed5e67f08";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response){
 
-        console.log(response);
-      })
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function(response){
+
+    //     console.log(response);
+    //   })
+    for (var i = 0; i < weatherInfo.length; i++){
+
+    }
 });
